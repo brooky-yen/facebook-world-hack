857a6b1409457ba0bb93bfa82881b9fb662652b2
Kai Kao <kai@eztable.com.tw>
2012-09-11 18:13:22 +0800

    mv files

--- public/index.php
+++ public/index.php
@@ -30,5 +30,49 @@ if (empty($data["user_id"])) {
                 */
 } 
 
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>wish tables</title>
+</head>
+<body>
+
+<div id="user_wish_tables">
+    <div id="wish_table_template" style="display: none;">
+        <img src="#" alt="Kai Kao" />
+        <p>Kai Kao 想吃的餐廳</p>
+        <ul style="display: none;">
+            <li><a href="#" target="_blank">Kai Kao餐廳</a></li>
+            <li><img src="#" width="130px" height="85px" alt="Kai Kao restaurant" /></li>
+        </ul>
+    </div>
+</div>
+
+
+<div id="union_wish_tables">
+    <p>你們想吃的全部餐廳</p>
+    <ul style="display: none;">
+        <li><a href="#" target="_blank">Kai Kao餐廳</a></li>
+        <li><img src="#" width="130px" height="85px" alt="Kai Kao restaurant" /></li>
+    </ul>
+</div>
+
+
+<div id="intersection_wish_tables">
+    <p>你們都想吃的餐廳</p>
+    <ul style="display: none;">
+        <li><a href="#" target="_blank">Kai Kao餐廳</a></li>
+        <li><img src="#" width="130px" height="85px" alt="Kai Kao restaurant" /></li>
+    </ul>
+</div>
+
+<script type="text/javascript" src="js/lib/jquery-1.8.1.min.js"></script>
+<script type="text/javascript" src="js/lib/jquery-1.8.1.min.js"></script>
+<script type="text/javascript" src="js/index.inc.js"></script>
+</body> 
+</html>
+
 
 

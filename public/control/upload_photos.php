91d3d5a3932396a175e29e68e551e5c33db5cb14
Kai Kao <kai@eztable.com.tw>
2012-09-11 15:14:06 +0800

    upload files

--- public/control/upload_photos.php
+++ public/control/upload_photos.php
@@ -1,163 +1,70 @@
 <?php
 session_start();
 
-$mysqli = new mysqli('http://socialtable.eztable.com/', 'socialtable', 'social_table', 'photos');
+// set photo path
+$PHOTO_PATH = '../photo/';
+
+// get $_FILES
+$FILES = $_FILES;
+
+// check files
+if (!isset($FILES['uploadedfile'])) {
+    echo "no files!";
+    exit;
+}
+if ($FILES['uploadedfile']['error'] > 0) {
+    echo 'something wrong...';
+    exit;
+}
+
+// connect mysqli
+$mysqli = new mysqli('localhost', 'social_table', 'socialtable', 'social_table');
 if (mysqli_connect_errno()) {
 	printf("Connect failed: %s\n", mysqli_connect_error());
 	exit;
 }
 
-var_dump($mysqli);exit;
-// check is this account already exist
-$query = 'SELECT * FROM `member` WHERE `account` = "' . $account . '"';
-$result = $mysqli->query($query);
-$resultArray = mysqli_fetch_assoc($result);
+// get temp path
+$tmpPath = $FILES["uploadedfile"]["tmp_name"];
 
-// if already exist, alert and redirect to last page
-if ($resultArray) {
-	$result->free();
-	$mysqli->close();
-	echo "<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />";
-	echo "<script>alert('This account already exist! Change other accounts'); history.back();</script>";
-	exit;
-}
-
-// get now datetime
+// add photo row
 $cdate = new DateTime();
 $cdate = $cdate->format('Y-m-d H:i:s');
 
-// insert member data
-$query = 'INSERT INTO `imigisco_kai`.`member` (';
-$query .= '`account`, `password`, `name`, `tel`, `email`, `cdate`)';
-$query .= 'VALUES ("' . $account . '","' . $_REQUEST['password'] . '","' . $_REQUEST['name'] . '","' . $_REQUEST['tel'] . '","' . $_REQUEST['email'] . '","' . $cdate . '");';
+// insert
+$query = 'INSERT INTO `social_table`.`photos` (';
+$query .= '`access_token`, `description`, `restaurant_id`, `cdate`)';
+$query .= 'VALUES ("' . $_REQUEST['access_token'] . '","' . $_REQUEST['description'] . '","' . $_REQUEST['restaurant_id'] . '","' . $cdate . '");';
 
 $result = $mysqli->query($query);
 $insertId = mysqli_insert_id($mysqli);
 
-// get member data
-$query = 'SELECT * FROM `member` WHERE `id` = ' . $insertId;
-$result = $mysqli->query($query);
-$member = mysqli_fetch_assoc($result);
+// get file name
+$fileName = $insertId . '.jpg';
 
-// check exception
-if ($insertId < 1 || !$member) {
-	$result->free();
-	$mysqli->close();
-	echo "<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />";
-	echo "<script>alert('Insert Faield!'); history.back();</script>";
-	exit;
+// get target path
+$targetPath = $PHOTO_PATH . $fileName;
+
+// upload photo
+$isUpload = move_uploaded_file($tmpPath, $targetPath);
+
+// update photo status by upload status
+if ($isUpload) {
+    // change permission
+    chmod($targetPath, 0666);
+    $status = 'ok';
+} else {
+    $status = 'failed';
 }
 
-// free connection
+// generate update query
+$updateQuery = 'UPDATE  `social_table`.`photos` SET  `upload_status` =  "' . $status . '" WHERE  `photos`.`id` =' . $insertId;
+
+// update
+$result = $mysqli->query($updateQuery);
+
+// free
 $result->free();
 $mysqli->close();
 
-
-
-
-
-
-/*
-
-// get restaurant id & reservation id
-$restaurantId = filter_input(INPUT_POST, CC::RESTAURANT_ID, FILTER_VALIDATE_INT);
-$reservationId = filter_input(INPUT_POST, 'br_id', FILTER_VALIDATE_INT);
-if ($restaurantId === false || $reservationId === false) {
-    echo htmlspecialchars(json_encode(false), ENT_NOQUOTES);
-    exit;
-}
-
-// get reservation
-$reservation = ReservationDao::getInstance()->getData(intval($reservationId));
-
-// get title
-$title = '';
-if ($reservation !== false) {
-    $title = $reservation[CC::SUBJECT];
-    if (trim($title) == '') {
-        $title = $reservation['perpose'];
-    }
-}
-
-// get account
-$app = EZApp::getInstance();
-$account = $app->getAccount();
-
-// create restaurant photo api
-$restaurantPhotoAPI = new RestaurantPhotoAPI($account);
-
-// get $_FILES
-$FILES = $app->getFILES();
-if (!isset($FILES['file'])) {
-    throw new EZParameterException();
-}
-
-// get temp path
-$tmpPaths = $FILES["file"]["tmp_name"];
-
-$isUpload = false;
-$photos = array();
-foreach ($tmpPaths as $tmpPath) {
-    // add photo
-    $photoId = $restaurantPhotoAPI->add_photo($restaurantId);
-    if (intval($photoId) <= 0) {
-        continue;
-    }
-
-    // update photo secret
-    $secret = StringUtils::randomAlphaNumericString(40);
-    $data = array(
-        CC::SECRET => $secret,
-        CC::RESERVATION_ID => $reservationId,
-        CC::TITLE => $title,
-    );
-    $updateRow = RestaurantPhotosDao::getInstance()->update($data, array(CC::ID => $photoId));
-
-    // get photo
-    $photo = RestaurantPhotosDao::getInstance()->getData($photoId);
-    if ($photo === false) {
-        continue;
-    }
-
-    // get file name
-    $fileName = RestaurantPhotosDao::getInstance()->getPhotoFilename($photo);
-
-    // get target path
-    $targetPath = RestaurantPhotosDao::IMG_TARGET_PATH . $fileName;
-
-    // move file to queue
-    $isMoveFile = move_uploaded_file($tmpPath, $targetPath);
-    if ($isMoveFile) {
-        // change permission
-        chmod($targetPath, 0666);
-        
-        // update photo upload status to queued
-        RestaurantPhotosDao::getInstance()->update(array(
-            CC::UPLOAD_STATUS => CV::QUEUED,
-        ), array(
-            CC::ID => $photo[CC::ID],
-        ));
-    
-        $isUpload = true;
-        $photos[] = array(
-            CC::ID => $photo[CC::ID],
-            'img' => $fileName,
-        );
-    } else {
-        // update photo upload status to failed
-        RestaurantPhotosDao::getInstance()->update(array(
-            CC::UPLOAD_STATUS => CV::FAILED,
-        ), array(
-            CC::ID => $photo[CC::ID],
-        ));
-    }
-    
-}
-
-if ($isUpload == true) {
-    echo json_encode($photos);
-} else {
-    echo json_encode(false);
-}
-*/
 exit;

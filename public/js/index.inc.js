4940ba5657de6a306450272f1e401e43672b7ea5
Kai Kao <kai@eztable.com.tw>
2012-09-11 18:36:16 +0800

    fixed bug

--- public/js/index.inc.js
+++ public/js/index.inc.js
@@ -5,7 +5,41 @@ $(document).ready(function() {
         cookie: true,
         oauth: true,
     });
+    FB.getLoginStatus(function(response) {
+        if (response.session) {
+          init();
+        } else {
+          // no user session available, someone you dont know
+        }
+    });
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*
+    FB.init({
+        appId: "131536600275003", 
+        status: true, 
+        cookie: true,
+        oauth: true,
+    });
     $("#jfmfs-container").jfmfs();
+    */
+    
+    
+    
+    
+    
     
     $('#fb-login').click(function() {
         postToFeed();
@@ -22,6 +56,49 @@ function loginFB() {
         }
     });
 }
+
+
+function login() {
+    FB.login(function(response) {
+        if (response.session) {
+            init();
+        } else {
+            alert('Login Failed!');
+        }
+    });
+}
+
+function init() {
+  FB.api('/me', function(response) {
+      $("#username").html("<img src='https://graph.facebook.com/" + response.id + "/picture'/><div>" + response.name + "</div>");
+
+
+
+      $("#jfmfs-container").jfmfs({ 
+          max_selected: 15, 
+          max_selected_message: "{0} of {1} selected",
+          friend_fields: "id,name,last_name",
+          pre_selected_friends: [1014025367],
+          exclude_friends: [1211122344, 610526078],
+          sorter: function(a, b) {
+            var x = a.last_name.toLowerCase();
+            var y = b.last_name.toLowerCase();
+            return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+          }
+      });
+      $("#jfmfs-container").bind("jfmfs.friendload.finished", function() { 
+          window.console && console.log("finished loading!"); 
+      });
+      $("#jfmfs-container").bind("jfmfs.selection.changed", function(e, data) { 
+          window.console && console.log("changed", data);
+      });                     
+      
+      $("#logged-out-status").hide();
+      $("#show-friends").show();
+
+  });
+}              
+
 
 function postToFeed() {
     var obj = {

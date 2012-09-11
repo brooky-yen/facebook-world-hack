ef4a9ecd579db552797b545597a3478450ce2a24
Kai Kao <kai@eztable.com.tw>
2012-09-11 16:59:19 +0800

    get wish tables

---
+++ public/js/wish_tables.inc.js
@@ -0,0 +1,63 @@
+$wishTables = {};
+
+$wishTables.getWishTablesByFacebookId = function() {
+    
+    // set api info
+    var url = "http://api.eztable.com/v2/user/get_want_to_eat_restaurants_by_facebook_id/";
+    var params = {
+        facebook_id: ['1569883047', '100000455848753']
+    };
+    
+    // call api to get data
+    $.ajax({
+        url: url + '?jsoncallback=?',
+        dataType: 'json',
+        data: params,
+        type: "POST",
+        success: function(returnValue) {
+            if (returnValue.status != 'OK') {
+                alert('get wish tables failed!');
+                return false;
+            }
+            
+            var data = returnValue.data;
+            var members = data.members;
+            $.each(members, function(index, member) {
+                var template = $('#wish_table_template').clone();
+                
+                // rm unnecessary attribute
+                template.removeAttr('id');
+                
+                // member name
+                template.find('p:first').text(member.real_name);
+                
+                // member img
+                var member_img = 'http://graph.facebook.com/' + member.facebook_id + '/picture?type=large';
+                template.find('img:first').attr('src', member_img).attr('alt', member.real_name);
+                
+                // process wish tables...
+                var tables = member.restaurants;
+                $.each(tables, function(i, table) {
+                    var ul = template.find('ul:first').clone();
+                    // table name
+                    ul.find('li:first').text(table.name);
+                    
+                    // table img
+                    var table_img = 'http://www.eztable.com.tw' + table.thumb1;
+                    ul.find('li:last img').attr('src', table_img).attr('alt', table.name);
+                    
+                    // append
+                    ul.show();
+                    template.append(ul);
+                });
+                
+                template.show();
+                $('#user_wish_tables').append(template);
+            });
+        }
+    });
+}
+
+$(document).ready(function() {
+    $wishTables.getWishTablesByFacebookId();
+});
---
+++ public/wish_tables.php
@@ -0,0 +1,26 @@
+<?php
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>wish tables</title>
+</head>
+<body>
+
+<div id="user_wish_tables">
+    <div id="wish_table_template" style="display: none;">
+        <img src="#" alt="Kai Kao" />
+        <p>Kai Kao 想吃的餐廳</p>
+        <ul style="display: none;">
+            <li>Kai Kao餐廳</li>
+            <li><img src="#" width="130px" height="85px" alt="Kai Kao restaurant" /></li>
+        </ul>
+    </div>
+</div>
+
+
+<script type="text/javascript" src="js/lib/jquery-1.8.1.min.js"></script>
+<script type="text/javascript" src="js/wish_tables.inc.js"></script>
+</body> 
+</html>

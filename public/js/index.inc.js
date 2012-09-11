ecf21830fdc94d144d0bf55519c438b5218f9fb5
Kai Kao <kai@eztable.com.tw>
2012-09-11 18:46:13 +0800

    fixed bug

--- public/js/index.inc.js
+++ public/js/index.inc.js
@@ -1,10 +1,25 @@
 $(document).ready(function() {
+    
     FB.init({
         appId: "131536600275003", 
         status: true, 
         cookie: true,
         oauth: true,
     });
+    
+    FB.login(function(response) {
+        if (response.authResponse) {
+            $("#jfmfs-container").jfmfs();
+          console.log('Welcome!  Fetching your information.... ');
+          FB.api('/me', function(response) {
+            console.log('Good to see you, ' + response.name + '.');
+          });
+        } else {
+          console.log('User cancelled login or did not fully authorize.');
+        }
+      });
+    
+    /*
     FB.getLoginStatus(function(response) {
         if (response.session) {
           //init();
@@ -13,10 +28,10 @@ $(document).ready(function() {
           // no user session available, someone you dont know
         }
     });
-
+    $("#jfmfs-container").jfmfs();
 
     
-    
+    */
     
     
     

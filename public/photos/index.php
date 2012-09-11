dc1f8fedd2f1289958e01190669e6dbbefd74318
Brooky Yen <brooky@eztable.com.tw>
2012-09-11 15:59:49 +0800

    add photos/index.php

--- public/photos/index.php
+++ public/photos/index.php
@@ -15,10 +15,24 @@ $photo = $result->fetch_assoc();
 
 
 <html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>Photo <?php echo $photo[$i]; ?></title>
+    <link href="../css/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css"  />
+</head>
 <body>
+
     <img src="photo/<?php echo $photo['id']; ?>.jpg" alt="" />
-<div class="description">
-    <?php  echo $photo['description']; ?>
-</div>
+    <div class="description">
+        <?php  echo $photo['description']; ?>
+    <div class="want_to_eat">
+        <iframe src="http://www.eztable.com.tw/widget/want_to_eat/eat.php?restaurant_id=<?php echo $photo['restaurant_id']; ?>" width="150" height="40"></iframe>
+    </div>
+    </div>
+      <button class="btn btn-large btn-primary" type="button">Login</button>
+
 </body>
+<script type="text/javascript" charset="utf-8">
+     
+</script>
 </html>

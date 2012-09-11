98651835029201c2af8c2dfb1245fe8c4faeee15
Kai Kao <kai@eztable.com.tw>
2012-09-11 18:34:04 +0800

    fixed bug

--- public/index.php
+++ public/index.php
@@ -27,10 +27,10 @@ if (empty($data["user_id"])) {
 */ 
 
 ?>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <link rel="stylesheet" href="js/lib/fb-friend-selector/jquery.facebook.multifriend.select.css" /> 
 <script src="http://connect.facebook.net/en_US/all.js"></script> 
 <title>wish tables</title>

b4df52be68684db75455125d0db6b309cd1670e4
Kai Kao <kai@eztable.com.tw>
2012-10-19 17:22:07 +0800

    test

--- public/post_action.php
+++ public/post_action.php
@@ -6,26 +6,14 @@ $facebook = new \Facebook(array(
     'secret' => '750fbfdd13c87817c5fa9c3d0843217c',
 ));
 
-
-
 $user_id = $facebook->getUser();
 if($user_id > 0) {
     try {
-       // $ret_obj = $facebook->api('/feed', 'POST', array(
-    //    	'link' => 'http://share.eztable.com.tw/page/share/292846/',
-    //    	'message' => '我在 EZTABLE Share Shopping 買了超棒的餐券！',
-    //    ));
-        
-        
         $params = array('voucher'=>'http://socialtable.eztable.com/public/post_action.php','access_token'=>$facebook->getAccessToken());
         $ret_obj = $facebook->api('/me/social_table:get', 'post', $params);
         
         var_dump($ret_obj);
-  
-       // $ret_obj = $facebook->api('/me/local_shareshopping:cook?recipe=http://share.eztable.com.tw/product/coupon/77/', 'POST', array(
-    //    	'link' => 'http://share.eztable.com.tw/page/share/292846/',
-  //      	'message' => '我在 EZTABLE Share Shopping 買了超棒的餐券！',
-    //    ));
+
         
     } catch (FacebookApiException $e) {
         var_dump($e);
@@ -35,73 +23,6 @@ if($user_id > 0) {
 
 
 
-
-
-
-
-
-/*
-
-$attachment =  array(
-    'access_token' => $accessToken,
-    'message' => 'test',
-    'name' => 'test title',
-    'link' => 'http://socialtable.eztable.com/public/post_action.php',
-    'description' => 'la l al al al ~',
-    'picture'=> 'http://www.thewordsworthhotel.co.uk/wp-content/uploads/gift-voucher.jpg',
-    'actions' => json_encode(array('name' => 'get','link' => 'http://socialtable.eztable.com/public/post_action.php'))
-);
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,'https://graph.facebook.com/fbnameorid/feed');
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $attachment);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  //to suppress the curl output 
-$result = curl_exec($ch);
-curl_close ($ch);
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-        /*
-        
-        $toURL = "https://graph.facebook.com/me/local_shareshopping:cook";
-$post = array(
-  "recipe"=>"http://localhost:10080/",
-  "access_token"=>"AAAC45NVxdQABAO3ZBmS3U2anRZAs3C0ZBP6UsIFyZB3PBgtSaMdMut6WNFaeFZB8oCZAdyExtGpcnRMQqZCyOwtm9WQfe0FSuUZD",
-    "method" => "post",
-);
-$ch = curl_init();
-$options = array(
-  CURLOPT_URL=>$toURL,
-  CURLOPT_HEADER=>0,
-  CURLOPT_VERBOSE=>0,
-  CURLOPT_RETURNTRANSFER=>true,
-  CURLOPT_USERAGENT=>"Mozilla/4.0 (compatible;)",
-  CURLOPT_POST=>true,
-  CURLOPT_POSTFIELDS=>http_build_query($post),
-);
-curl_setopt_array($ch, $options);
-// CURLOPT_RETURNTRANSFER=true 會傳回網頁回應,
-// false 時只回傳成功與否
-$result = curl_exec($ch); 
-curl_close($ch);
-echo $result;
-*/
-        
-        
-        // kaikao
         
        
 
